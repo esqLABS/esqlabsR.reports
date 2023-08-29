dff33398c72a4de9ee0d68d3fa3e575c3dcdf837
Felix MIL <felix.mil@esqlabs.com>
2023-08-29 14:07:33 +0200

    Add function documentation

--- R/new_report.R
+++ R/new_report.R
@@ -1,12 +1,12 @@
 #' Create a new report
 #'
-#' @param file_name
-#' @param path
+#' @param file_name a string that represent the name of the report that will be created.
+#' @param path a string that represent the path where to create the report.
 #'
-#' @return
 #' @export
 #'
 #' @examples
+#' new_report("my_report", "report_folder/")
 new_report <- function(file_name, path = NA) {
   fs::file_copy(
     path = system.file("templates", "esqlabsR.reports.qmd", package = "esqlabsR.reports"),
